9646110895373fd75f121bf922d127a92610de7d
John Doe <johndoe@example.com>
2022-09-03 00:00:40 +0800

    [init]learn how to use props

---
+++ src/components/greeting.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+// Defining the <Greeting> component
+export const Greeting = (props) =>{
+    return(
+        <p>Hi {props.name}</p>
+    )
+}
---
+++ src/pages/say-hello.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import {Greeting} from '../components/greeting'
+const SayHello = ()=>{
+    return(
+        <div>
+            <Greeting name="Megan"/>
+        </div>
+    )
+}
+export default SayHello
