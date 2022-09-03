9996462396b13a26e8ac3ce74bd5931308573e16
John Doe <johndoe@example.com>
2022-09-03 18:08:43 +0800

    [init]add css for header

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@ import {
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle
 } from "./layout.module.css";
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -19,7 +20,7 @@ const Layout = ({ pageTitle, children }) => {
   `);
   return (
     <div className={container}>
-      <header>{data.site.siteMetadata.title}</header>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
