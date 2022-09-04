3c06404b4941b8afd8ad4b99387e3f34d5764dc1
John Doe <johndoe@example.com>
2022-09-04 10:52:14 +0800

    [init]use mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,16 @@ module.exports = {
     title: "My First Gatsby Site",
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp",{
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: `blog`,
-      path: `${__dirname}/blog`,
-    }
-  },],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,8 +7,12 @@ const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
+        {data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))}
       </ul>
     </Layout>
@@ -17,9 +21,14 @@ const BlogPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx {
       nodes {
-        name
+        id
+        frontmatter {
+          date(formatString: "MMMM DD YYYY")
+          title
+        }
+        excerpt
       }
     }
   }
